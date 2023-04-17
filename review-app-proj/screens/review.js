900b2e6f08b1b4d2141b54173fcf6034f6d14185
falakn2405 <falakn2405@gmail.com>
2023-04-18 00:43:24 +0500

    Update review.js

--- review-app-proj/screens/review.js
+++ review-app-proj/screens/review.js
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function Review() {
     return (
         <View style={styles.container}>
-            <Text>Revie Screen</Text>
+            <Text>Revie Screen </Text>
         </View>
     )
 }
