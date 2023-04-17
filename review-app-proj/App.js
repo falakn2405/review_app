e0787b2b08410b9ecb1ac56eabaf5c3dea3bc673
falakn2405 <falakn2405@gmail.com>
2023-04-18 00:42:18 +0500

    First Page

--- review-app-proj/App.js
+++ review-app-proj/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{backgroundColor: 'red', flex: 0.15}}>
+        <Text style={{marginTop: 20, fontSize: 32, textAlign: 'center', verticalAlign: 'middle'}}>Spectacles</Text>
+        <Text style={{fontSize: 16, textAlign: 'center', verticalAlign: 'middle'}}>Review Mobile App</Text>
+      </View>
+      <View style={{backgroundColor: 'beigo', flex: 0.55}}>
+        <Text style={{marginTop: 25, padding: 30, backgroundColor: 'darkgrey', fontSize: 24}}>Home</Text>
+        <Text style={{marginTop: 25, padding: 30, backgroundColor: 'darkgrey', fontSize: 24}}>About</Text>
+        <Text style={{marginTop: 25, padding: 30, backgroundColor: 'darkgrey', fontSize: 24}}>Review</Text>
+      </View>
+      <View style={{backgroundColor: 'beigo', flex: 0.30}}>
+        <Text style={{marginTop: 20, padding: 17, backgroundColor: 'grey', textAlign: 'center', fontSize: 20}}>LogIn</Text>
+        <Text style={{marginTop: 20, padding: 17, backgroundColor: 'grey', textAlign: 'center', fontSize: 20}}>SignUp</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+
--- review-app-proj/screens/review.js
+++ review-app-proj/screens/review.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function Review() {
     return (
-        <View style={StyleSheet.container}>
+        <View style={styles.container}>
             <Text>Revie Screen</Text>
         </View>
     )
