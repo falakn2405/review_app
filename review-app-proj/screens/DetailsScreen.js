39d08dff1d12bb1f1c92db7336a9027936ce2024
falakn2405 <falakn2405@gmail.com>
2023-05-09 10:36:02 +0500

    Screens added

---
+++ review-app-proj/screens/DetailsScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const DetailsScreen = ({navigation}) => {
+    return (
+      <View style={styles.container}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to home"
+          onPress={() => navigation.navigate("Home")}
+        />
+      </View>
+    );
+};
+
+export default DetailsScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
