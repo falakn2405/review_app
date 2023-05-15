f36a57a760d41b2e1b5d0fff3c450271c182051e
falakn2405 <falakn2405@gmail.com>
2023-05-15 17:36:31 +0500

    LogIn & SignUp

--- review-app-proj/App.js
+++ review-app-proj/App.js
@@ -3,19 +3,21 @@ import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { DrawerContent } from './screens/DrawerContent';
 
 import MainTabScreen from './screens/MainTabScreen';
-import { DrawerContent } from './screens/DrawerContent';
+import RootStackScreen from './screens/RootStackScreen';
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>
+      <RootStackScreen/>
+      {/* <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>
         <Drawer.Screen name="Review-App" component={MainTabScreen} />
-        {/* <Drawer.Screen name="Details" component={DetailsStackScreen} /> */}
-      </Drawer.Navigator>
+        
+      </Drawer.Navigator> */}
     </NavigationContainer>
   );
 }
