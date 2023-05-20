b11da302f12c21670a6b0f9e765a7c4d938000b1
falakn2405 <falakn2405@gmail.com>
2023-05-20 17:33:57 +0500

    Reviews Detail

--- review-app-proj/screens/DetailsScreen.js
+++ review-app-proj/screens/DetailsScreen.js
@@ -1,24 +1,15 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { globalStyles } from "../styles/global";
 
-const DetailsScreen = ({navigation}) => {
-    return (
-      <View style={styles.container}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to home"
-          onPress={() => navigation.navigate("Home")}
-        />
-      </View>
-    );
+const DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={globalStyles.container}>
+      <Text>{ navigation.getParam('title') }</Text>
+      <Text>{ navigation.getParam('body') }</Text>
+      <Text>{ navigation.getParam('rating') }</Text>
+    </View>
+  );
 };
 
 export default DetailsScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
