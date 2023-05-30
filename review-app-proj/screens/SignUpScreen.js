25e54fe415d5f7e44c1e0e92f6a92d4b3fe2391f
falakn2405 <falakn2405@gmail.com>
2023-05-30 12:14:38 +0500

    SignUp Page

--- review-app-proj/screens/SignUpScreen.js
+++ review-app-proj/screens/SignUpScreen.js
@@ -1,12 +1,156 @@
 import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Dimensions, Platform } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+import { set } from 'react-native-reanimated';
+import { StatusBar } from 'expo-status-bar';
 
-const SignUpScreen = () => {
+const SignUpScreen = ({navigation}) => {
+    const [data, setData] = React.useState({
+        email: '',
+        password: '',
+        check_textInputChange: false,
+        secureTextEntry: true
+    });
+    const textInputChange = (val) => {
+        if(val.length != 0) {
+            setData({
+                ...data,
+                email: val,
+                check_textInputChange: true
+            });
+        } else {
+            setData({
+                ...data,
+                email: val,
+                check_textInputChange: false
+            });
+        }
+    }
+    const handlePasswordChange = (val) => {
+        setData({
+            ...data,
+            password: val
+        });
+    }
+    const updateSecureTextEntry = () => {
+        setData({
+            ...data,
+            secureTextEntry: !data.secureTextEntry
+        })
+    }
+
     return (
         <View style={styles.container}>
-            <Text>SignUpScreen</Text>
-            <Button title='Click Here' 
-                onPress={() => alert('Button Clicked')} />
+            <StatusBar backgroundColor='#009387' barStyle='light-conten' />
+            {/* Header */}
+            <View style={styles.header}>
+                <Text style={styles.text_header}>Register!</Text>
+            </View>
+            {/* Footer */}
+            <View style={styles.footer}>
+
+                {/* Email tag */}
+                <Text style={styles.text_footer}>Email</Text>
+                <View style={styles.action}>
+                    <FontAwesome
+                        name='user-o'
+                        color='#05375a'
+                        size={20}
+                    />
+                    <TextInput
+                        placeholder='Your Email'
+                        style={styles.textInput}
+                        autoCapitalize='none'
+                        onChangeText={(val)=>textInputChange(val)}
+                    />
+                    {data.check_textInputChange ? 
+                    <Feather 
+                        name='check-circle'
+                        color='green'
+                        size={20}
+                    />
+                    : null }
+                </View>
+                
+                {/* Password tag with show and hide */}
+                <Text style={[styles.text_footer, {marginTop: 30}]}>Password</Text>
+                <View style={styles.action}>
+                    <Feather
+                        name='lock'
+                        color='#05375a'
+                        size={20}
+                    />
+                    <TextInput
+                        placeholder='Your Password'
+                        secureTextEntry={data.secureTextEntry ? true : false}
+                        style={styles.textInput}
+                        autoCapitalize='none'
+                        onChangeText={(val)=>handlePasswordChange(val)}
+                    />
+                    <TouchableOpacity onPress={updateSecureTextEntry}>
+                        {data.secureTextEntry ?
+                        <Feather 
+                            name='eye-off'
+                            color='grey'
+                            size={20}
+                        />
+                        :
+                        <Feather 
+                            name='eye'
+                            color='grey'
+                            size={20}
+                        />
+                        }
+                    </TouchableOpacity>
+                </View>
+                <Text style={[styles.text_footer, {marginTop: 30}]}>Confirm Password</Text>
+                <View style={styles.action}>
+                    <Feather
+                        name='lock'
+                        color='#05375a'
+                        size={20}
+                    />
+                    <TextInput
+                        placeholder='Your Password'
+                        secureTextEntry={data.secureTextEntry ? true : false}
+                        style={styles.textInput}
+                        autoCapitalize='none'
+                        onChangeText={(val)=>handlePasswordChange(val)}
+                    />
+                    <TouchableOpacity onPress={updateSecureTextEntry}>
+                        {data.secureTextEntry ?
+                        <Feather 
+                            name='eye-off'
+                            color='grey'
+                            size={20}
+                        />
+                        :
+                        <Feather 
+                            name='eye'
+                            color='grey'
+                            size={20}
+                        />
+                        }
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.button}>
+                    {/* SignIn button to naviagte to home page */}
+                    <LinearGradient colors={['#08d4c4', '#01ab9d']} 
+                        style={styles.signIn} >
+                        <Text style={[styles.textSign, {color:'#fff'}]}>
+                            Sign In</Text>
+                    </LinearGradient>
+
+                    {/* SignUp button to register account */}
+                    <TouchableOpacity onPress={()=>navigation.navigate('SignUpScreen')}
+                        style={[styles.signIn, {borderColor:'#009387', borderWidth: 1, marginTop: 15}]}
+                    >
+                        <Text st={[styles.textSign, {color: '#009387'}]}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
@@ -15,8 +159,69 @@ export default SignUpScreen;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+      flex: 1, 
+      backgroundColor: '#009387',
     },
+    header: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        paddingHorizontal: 20,
+        paddingBottom: 50
+    },
+    footer: {
+        flex: 3,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingHorizontal: 20,
+        paddingVertical: 30
+    },
+    text_header: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    text_footer: {
+        color: '#05375a',
+        fontSize: 18
+    },
+    action: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+        paddingBottom: 5
+    },
+    actionError: {
+        flexDirection: 'row',
+        marginTop: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#FF0000',
+        paddingBottom: 5
+    },
+    textInput: {
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? 0 : -12,
+        paddingLeft: 10,
+        color: '#05375a',
+    },
+    errorMsg: {
+        color: '#FF0000',
+        fontSize: 14,
+    },
+    button: {
+        alignItems: 'center',
+        marginTop: 50
+    },
+    signIn: {
+        width: '100%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10
+    },
+    textSign: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
 });
