d96a4d39e460b787f067260a3c356cd0e262c3ee
falakn2405 <falakn2405@gmail.com>
2023-06-05 21:27:48 +0500

    Update card.js

--- review-app-proj/shared/card.js
+++ review-app-proj/shared/card.js
@@ -5,12 +5,14 @@ import { globalStyles } from "../styles/global";
 const Card = (props) => {
     return (
         <View style={globalStyles.card}>
+            {/* Outer container for the card */}
             <View style={globalStyles.cardContent}>
+                {/* Inner container for the card content */}
                 { props.children }
+                {/* Content rendered inside the card */}
             </View>
         </View>
     );
 };
 
 export default Card;
-
