ff47305b09a7cb70ba0c0ecb68fc860911e1740d
falakn2405 <falakn2405@gmail.com>
2023-06-05 21:28:31 +0500

    Update DrawerContent.js

--- review-app-proj/screens/DrawerContent.js
+++ review-app-proj/screens/DrawerContent.js
@@ -1,86 +1,76 @@
 import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Avatar, Title, Caption, Paragraph, Drawer, 
-    Text, TouchableRipple, Switch } from 'react-native-paper';
+import { Avatar, Title, Caption, Paragraph, Drawer, Text, TouchableRipple, Switch } from 'react-native-paper';
 import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { AuthContext } from '../components/context';
-import ExploreScreen from './ExploreScreen';
 
 export function DrawerContent(props) {
     const [isDarkTheme, setIsDarkTheme] = React.useState(false);
-    const {signOut} = React.useContext(AuthContext);
+    const { signOut } = React.useContext(AuthContext);
+
+    // Function to toggle between light and dark theme
     const toggleTheme = () => {
         setIsDarkTheme(!isDarkTheme);
-    }
+    };
 
-    return(
-        <View style={{flex:1}}>
+    return (
+        <View style={{ flex: 1 }}>
+            {/* Scrollable content of the drawer */}
             <DrawerContentScrollView {...props}>
                 <View style={styles.drawerContent}>
                     <View style={styles.userInfoSection}>
-                        <View style={{flexDirection:'row', marginTop:15}}>
-                            <Avatar.Image 
-                                source={{uri: 'https://cdn-icons-png.flaticon.com/512/6596/6596121.png'}}
-                                size={50} />
-                            <View style={{marginLeft:15, flexDirection:'column'}}>
+                        <View style={{ flexDirection: 'row', marginTop: 15 }}>
+                            {/* User avatar */}
+                            <Avatar.Image
+                                source={{ uri: 'https://cdn-icons-png.flaticon.com/512/6596/6596121.png' }}
+                                size={50}
+                            />
+                            <View style={{ marginLeft: 15, flexDirection: 'column' }}>
                                 <Title style={styles.title}>Falak Naz</Title>
                                 <Caption style={styles.caption}>@FN2405</Caption>
                             </View>
                         </View>
                     </View>
+                    {/* Drawer section with navigation items */}
                     <Drawer.Section style={styles.drawerSection}>
                         <DrawerItem
-                            icon={({color, size}) => (
-                                <MaterialCommunityIcons
-                                    name='home-outline'
-                                    color={color}
-                                    size={size}
-                                />
+                            icon={({ color, size }) => (
+                                <MaterialCommunityIcons name='home-outline' color={color} size={size} />
                             )}
                             label="Home"
-                            onPress={() => {props.navigation.navigate('Home')}}
+                            onPress={() => { props.navigation.navigate('Home') }}
                         />
                         <DrawerItem
-                            icon={({color, size}) => (
-                                <MaterialCommunityIcons
-                                    name='information-outline'
-                                    color={color}
-                                    size={size}
-                                />
+                            icon={({ color, size }) => (
+                                <MaterialCommunityIcons name='information-outline' color={color} size={size} />
                             )}
                             label="About"
-                            onPress={() => {props.navigation.navigate('Explore')}}
+                            onPress={() => { props.navigation.navigate('Explore') }}
                         />
                         <DrawerItem
-                            icon={({color, size}) => (
-                                <MaterialIcons
-                                    name='post-add'
-                                    color={color}
-                                    size={size}
-                                />
+                            icon={({ color, size }) => (
+                                <MaterialIcons name='post-add' color={color} size={size} />
                             )}
                             label="Review"
-                            onPress={() => {props.navigation.navigate('Review')}}
+                            onPress={() => { props.navigation.navigate('Review') }}
                         />
                         <DrawerItem
-                            icon={({color, size}) => (
-                                <MaterialIcons
-                                    name='settings-applications'
-                                    color={color}
-                                    size={size}
-                                />
+                            icon={({ color, size }) => (
+                                <MaterialIcons name='settings-applications' color={color} size={size} />
                             )}
                             label="Settings"
-                            onPress={() => {props.navigation.navigate('Setting')}}
+                            onPress={() => { props.navigation.navigate('Setting') }}
                         />
                     </Drawer.Section>
+                    {/* Drawer section for theme preference */}
                     <Drawer.Section>
-                        <TouchableRipple onPress={() => {toggleTheme()}}>
+                        <TouchableRipple onPress={() => { toggleTheme() }}>
                             <View style={styles.preference}>
                                 <Text>Dark Theme</Text>
                                 <View pointerEvents='none'>
+                                    {/* Toggle switch for theme */}
                                     <Switch value={isDarkTheme} />
                                 </View>
                             </View>
@@ -88,17 +78,15 @@ export function DrawerContent(props) {
                     </Drawer.Section>
                 </View>
             </DrawerContentScrollView>
+            {/* Drawer section for Sign Out */}
             <Drawer.Section style={styles.bottomDrawerSection}>
                 <DrawerItem
-                    icon={({color, size}) => (
-                        <MaterialCommunityIcons
-                            name='exit-to-app'
-                            color={color}
-                            size={size}
-                        />
+                    icon={({ color, size }) => (
+                        // Icon for Sign Out
+                        <MaterialCommunityIcons name='exit-to-app' color={color} size={size} />
                     )}
                     label="Sign Out"
-                    onPress={() => {signOut()}}
+                    onPress={() => { signOut() }}
                 />
             </Drawer.Section>
         </View>
